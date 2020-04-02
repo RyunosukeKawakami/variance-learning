edab835883846e2caa3bb1b8898335b1e63ba7f7
RyunosukeKawakami <kawadora28@gmail.com>
2020-04-02 13:22:36 +0900

    Detailページを作成

---
+++ src/components/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <navigation></navigation>
+    </div>
+</template>
+
+<script>
+import Navigation from '@/components/Navigation.vue'
+
+export default {
+    components:{
+        Navigation
+    }
+}
+</script>
+
+<style>
+
+</style>
